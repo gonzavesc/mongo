aa7c6ca85a69bf6d5e61c7ab0f20fe26e7ac14d3
Gonzalo <gonzalovescovi@gmail.com>
2020-03-26 16:38:18 +0100

    fixed errors and added to list

--- database.py
+++ database.py
@@ -29,10 +29,13 @@ while ok == 0:
         pass
 gsh = client.open('Comida')
 sheet = gsh.get_worksheet(0)
+sheet2 = gsh.get_worksheet(1)
 tod = datetime.date.today()
 day = "{}".format(tod)
 column1 = sheet.col_values(1)
 column2 = sheet.col_values(2)
+column2_5 = sheet2.col_values(5)
+column2_1 = sheet2.col_values(1)
 foods = int((len(sys.argv)-1)/2)
 for i in range(foods):
     print(i)
@@ -42,14 +45,23 @@ for i in range(foods):
     for x in mycol.find({'code' : codes},{'product_name' : 1,'nutriments':1}):
         print(x)
         print(x['nutriments']['energy_value'])
+        energ_100 = float(x['nutriments']['energy_value'])
+        energ = energ_100 * qtty / 100
+        fat_100 = float(x['nutriments']['fat_value'])
+        fat = fat_100 * qtty / 100
+        prot_100 = float(x['nutriments']['proteins_value'])
+        prot =  prot_100 * qtty / 100
+        carbs_100 = float(x['nutriments']['carbohydrates_value'])
+        carbs =  carbs_100 * qtty / 100
+        name = x['product_name']
+        print("The energy unit is {}".format(x['nutriments']['energy_unit']))
+        if x['nutriments']['energy_unit'] == "kJ":
+            energ = energ/4.184
+            energ_100 = energ_100 / 4.184
+            print("Its in kJ")
         if day not in column1:
-            L = len(column1)
+            L = len(column2)
             sheet.update_cell(L+1,1,day)
-            energ = float(x['nutriments']['energy_value']) * qtty / 100
-            fat = float(x['nutriments']['fat_value']) * qtty / 100
-            prot = float(x['nutriments']['proteins_value']) * qtty / 100
-            carbs = float(x['nutriments']['carbohydrates_value']) * qtty / 100
-            name = x['product_name']
             sheet.update_cell(L + 1, 3, qtty)
             sheet.update_cell(L + 1, 4, carbs)
             sheet.update_cell(L + 1, 5, fat)
@@ -72,11 +84,6 @@ for i in range(foods):
             prot_tot = float(sheet.cell(J + 1, 6).value)
             carbs_tot = float(sheet.cell(J + 1, 4).value)
             qtty_tot = float(sheet.cell(J + 1, 3).value)
-            energ = float(x['nutriments']['energy_value']) * qtty / 100
-            fat = float(x['nutriments']['fat_value']) * qtty / 100
-            prot = float(x['nutriments']['proteins_value']) * qtty / 100
-            carbs = float(x['nutriments']['carbohydrates_value']) * qtty / 100
-            name = x['product_name']
             
             sheet.update_cell(J + 1, 3, qtty + qtty_tot)
             sheet.update_cell(J + 1, 4, carbs + carbs_tot)
@@ -91,3 +98,11 @@ for i in range(foods):
             sheet.update_cell(L + 1, 6, prot)
             sheet.update_cell(L + 1, 7, energ)
             sheet.update_cell(L + 1, 8, codes)
+        if codes not in column2_5:
+            L = len(column2_1)
+            sheet2.update_cell(L + 1, 1, name)
+            sheet2.update_cell(L + 1, 2, energ_100)
+            sheet2.update_cell(L + 1, 3, prot_100)
+            ratio = prot_100 / energ_100 * 100
+            sheet2.update_cell(L + 1, 4, ratio)
+            sheet2.update_cell(L + 1, 5, codes)
